2b51cf51993a30c939e23b9128a3f33374761392
Kono <wedoza112@gmail.com>
2026-01-07 17:06:53 +0700

    Update L4-2.c

--- L4-2.c
+++ L4-2.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 
-void go( int ***p, int **z ) {
-
-    *p = z;
-
-    printf("--- Inside go function ---\n");
-    printf("Value being pointed to: %d\n", ***p); 
-    printf("--------------------------\n");
-}
+void go( int ***p, int **z ) ;
 
 int main() {
-    int *b = new int; *b = 10;
-    int *c = new int; *c = 20;
-    int **a;
+    int *b = new int ; *b = 10 ;
+    int *c = new int ; *c = 20 ;
+    int **a ;
 
-    go( &a, &b );
-    printf("%d %p %p %p\n", **a, *a, a, &a);
+    go( &a, &b ) ;
+    printf( "%d %p %p %p\n", **a, *a, a, &a ) ;
 
-    go( &a, &c );
-    printf("%d %p %p %p\n", **a, *a, a, &a);
+    go( &a, &c ) ;
+    printf( "%d %p %p %p\n", **a, *a, a, &a ) ;
 
-    delete b;
-    delete c;
+    return 0 ;
+}
 
-    return 0;
+void go( int ***p, int **z ) {
+
+    *p = z ;
 }
